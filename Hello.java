1ee4b4cc610cfcd3929d51c4a4e7de017a3f79fe
JPXC <69910506+JPXC@users.noreply.github.com>
2020-08-19 10:44:12 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/****************************************************
+* Hello.java
+**************
+* Joshua Pomeroy
+* August 19th, 2020
+* Intro to Programming 255 003
+****************************************************/
+
+	public class Hello{
+		public static void main(String[] args) {
+			System.out.println("Joshua Pomeroy");
+			System.out.println("<Hello.java>");
+			System.out.println("This program is to test the java application on this computer");
+			System.out.println("8/19/20");
+			System.out.println("<255 003>");
+			System.out.println();
+		}
+	}
