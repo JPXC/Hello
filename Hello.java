0733e8bdbcfc2bb4ab155166618b3cd2b6ff1747
JPXC <69910506+JPXC@users.noreply.github.com>
2021-01-27 13:50:33 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,16 +2,16 @@
 * Hello.java
 **************
 * Joshua Pomeroy
-* August 19th, 2020
+* January 27th, 2021
 * Intro to Programming 255 003
 ****************************************************/
 
 	public class Hello{
 		public static void main(String[] args) {
-			System.out.println("Joshua Pomeroy");
+			System.out.println("<Joshua Pomeroy>");
 			System.out.println("<Hello.java>");
-			System.out.println("This program is to test the java application on this computer");
-			System.out.println("8/19/20");
+			System.out.println("<This program is saying hello>");
+			System.out.println("<1/27/21>");
 			System.out.println("<255 003>");
 			System.out.println();
 		}
